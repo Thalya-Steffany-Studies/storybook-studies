e7953b28a35fda38c4839a996b4d885fc7332662
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-09-16 10:33:27 -0300

    docs(Button): cria documentação do componente

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,33 @@
+import { Button } from ".";
+import { Meta, StoryObj } from '@storybook/react';
+import { IButtonProps } from "./IButton";
+
+const meta: Meta = {
+    title: 'Components/Button',
+    component: Button,
+    argTypes: {
+        variant: {
+            control: {
+                type: 'select',
+                options: ['primary' , 'danger' , 'warning' , 'success']
+            },
+            defaultValue: 'primary'
+        },
+        size: {
+            control: {
+                type: 'radio',
+                options: ['sm' , 'md' , 'lg']
+            },
+            defaultValue: 'sm'
+        },
+        text: { control: 'text' }
+    }
+}
+export default meta;
+
+export const Default = (args: IButtonProps) => <Button {...args} />;
+Default.args = {
+    text: 'Button',
+    size: 'sm',
+    variant: 'primary'
+}
