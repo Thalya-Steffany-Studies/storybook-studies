fac4fe940532507e8b9d7130a9fb5006b112eeca
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-09-16 10:32:21 -0300

    feat: cria setup inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from './components/Button'
+
+function App() {
+
+  return (
+    <>
+      <Button size='sm' text='Button' variant='danger'/>
+    </>
+  )
+}
+
+export default App
